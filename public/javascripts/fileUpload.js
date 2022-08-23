ca97dfe5fc1742ff0a1b3cd839f8037171b0c8f3
Pham Thuan <pvthuan92@gmail.com>
2022-08-23 15:21:21 +0700

    I am John

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   FilePond.registerPlugin(FilePondPluginImagePreview);
   FilePond.registerPlugin(FilePondPluginFileEncode);
-  FilePond.registerPlugin(FilePondPluginImageCrop);
   FilePond.registerPlugin(FilePondPluginImageResize);
+  FilePond.registerPlugin(FilePondPluginImageCrop);
 
-  FilePond.setOptions({
-    stylePanelAspectRation: 150 / 100,
-    imageCropAspectRatio: 3 / 2,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150,
-  });
   const inputElement = document.querySelector('input[type="file"]');
   const pond = FilePond.create(inputElement);
+
+  pond.setOptions({
+    imageCropAspectRatio: "2:3",
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150,
+    imageResizeMode: "cover",
+  });
 });
--- routes/books.js
+++ routes/books.js
@@ -30,6 +30,25 @@ router.get("/new", async (req, res) => {
   renderNewPage(res, new Book());
 });
 
+//Edit book routes
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    renderEditPage(res, book);
+  } catch {
+    res.redirect("/");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id).populate("author").exec();
+    res.render("books/show", { book: book });
+  } catch {
+    res.redirect("/");
+  }
+});
+
 router.post("/", async (req, res) => {
   const fileName = req.file != null ? req.file.filename : null;
   const book = new Book({
@@ -44,14 +63,67 @@ router.post("/", async (req, res) => {
 
   try {
     const newBook = await book.save();
-    // res.redirect(`books/${newBook.id}`);
-    res.redirect(`books`);
+    res.redirect(`books/${newBook.id}`);
+    // res.redirect(`books`);
   } catch {
     renderNewPage(res, book, true);
   }
 });
+//Update Book routes
+router.put("/:id", async (req, res) => {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.publishDate = new Date(req.body.publishDate);
+    book.pageCount = req.body.pageCount;
+    book.description = req.body.description;
+
+    if (req.body.cover != null && req.body.cover !== "") {
+      saveCover(book, req.body.cover);
+    }
+
+    await book.save();
+    res.redirect(`/books/${book.id}`);
+  } catch {
+    if (book != null) {
+      renderEditPage(res, book, true);
+    } else {
+      res.redirect("/");
+    }
+  }
+});
 
 async function renderNewPage(res, book, hasError = false) {
+  renderFormPage(res, book, "new", hasError);
+}
+
+async function renderEditPage(res, book, hasError = false) {
+  renderFormPage(res, book, "edit", hasError);
+}
+//Delete Book
+
+router.delete("/:id", async (req, res) => {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    await book.remove();
+    res.redirect("/books");
+  } catch {
+    if (book != null) {
+      res.render("books/show", {
+        book: book,
+        errorMessage: "Could not remove Book",
+      });
+    } else {
+      res.redirect("/");
+    }
+  }
+});
+
+//render Form
+async function renderFormPage(res, book, form, hasError = false) {
   try {
     const authors = await Author.find({});
     const params = {
@@ -59,9 +131,13 @@ async function renderNewPage(res, book, hasError = false) {
       book: book,
     };
     if (hasError) {
-      params.errorMessage = "Error Creating Book";
+      if (form === "edit") {
+        params.errorMessage = "Error Updating Book";
+      } else {
+        params.errorMessage = "Error Creating Book";
+      }
     }
-    res.render("books/new", params);
+    res.render(`books/${form}`, params);
   } catch {
     res.redirect("/books");
   }
