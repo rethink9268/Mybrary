2351890d4db6d69c6bb44f628fca04bfb68d9358
Pham Thuan <pvthuan92@gmail.com>
2022-08-24 16:37:07 +0700

    Finish

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,4 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
+const rootStyles = window.getComputedStyle(document.documentElement);
+
+if (
+  rootStyles.getPropertyValue("--book-cover-width-large") != null &&
+  rootStyles.getPropertyValue("--book-cover-width-large") !== ""
+) {
+  ready();
+} else {
+  document.getElementById("main-css").addEventListener("load", ready);
+}
+
+function ready() {
+  const coverWidth = parseFloat(
+    rootStyles.getPropertyValue("--book-cover-width-large")
+  );
+  const coverAspectRation = parseFloat(
+    rootStyles.getPropertyValue("--book-cover-aspect-ratio")
+  );
+  const coverHeight = coverWidth / coverAspectRation;
+
   FilePond.registerPlugin(FilePondPluginImagePreview);
   FilePond.registerPlugin(FilePondPluginFileEncode);
   FilePond.registerPlugin(FilePondPluginImageResize);
@@ -8,10 +27,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const pond = FilePond.create(inputElement);
 
   pond.setOptions({
-    imageCropAspectRatio: "2:3",
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150,
+    imageCropAspectRatio: 1 / coverAspectRation,
+    stylePanelAspectRatio: 1 / coverAspectRation,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight,
     imageResizeMode: "cover",
   });
-});
+}
